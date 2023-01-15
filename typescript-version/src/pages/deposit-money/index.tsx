ae5294533c8c6dcceb180a7025f6c22fec1785ab
tuananhvd1998 <tuananhvd1998@gmail.com>
2023-01-15 13:57:20 +0700

    deposit

--- typescript-version/src/pages/deposit-money/index.tsx
+++ typescript-version/src/pages/deposit-money/index.tsx
@@ -1,4 +1,5 @@
 // ** MUI Imports
+import { CardContent, Typography } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import Grid from '@mui/material/Grid';
@@ -24,10 +25,16 @@ const DepositMoney = () => {
 
   return (
     <Grid container spacing={6}>
-      <Grid item xs={12}>
+      <Grid item xs={6}>
         <Card>
-          <CardHeader title='Nạp tiền' titleTypographyProps={{ variant: 'h6' }} />
+          <CardHeader title='Hướng dẫn nạp tiền vào tài khoản' titleTypographyProps={{ variant: 'h6' }} />
+          <CardContent>
+            <Typography variant='body1'>Để nạp tiền vào tài khoản vui lòng chuyển khoản theo thông tin sau:</Typography>
+          </CardContent>
         </Card>
+      </Grid>
+      <Grid item xs={6}>
+        <CardHeader title='Nạp tiền' titleTypographyProps={{ variant: 'h6' }} />
       </Grid>
     </Grid>
   );
