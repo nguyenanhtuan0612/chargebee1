b0ffe7b3544ba9a76a27475cc37d6e8d53dad980
ha.le <baopt>
2023-01-15 13:53:00 +0700

    update UI

--- typescript-version/src/layouts/components/vertical/AppBarContent.tsx
+++ typescript-version/src/layouts/components/vertical/AppBarContent.tsx
@@ -17,6 +17,7 @@ import { Settings } from 'src/@core/context/settingsContext';
 import ModeToggler from 'src/@core/layouts/components/shared-components/ModeToggler';
 import UserDropdown from 'src/@core/layouts/components/shared-components/UserDropdown';
 import NotificationDropdown from 'src/@core/layouts/components/shared-components/NotificationDropdown';
+import { auto } from '@popperjs/core';
 
 interface Props {
   hidden: boolean;
@@ -33,7 +34,18 @@ const AppBarContent = (props: Props) => {
   const hiddenSm = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
 
   return (
-    <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+    <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <Box className='actions-left' sx={{ mr: auto, display: 'flex', alignItems: 'center' }}>
+        {hidden ? (
+          <IconButton
+            color='inherit'
+            onClick={toggleNavVisibility}
+            sx={{ ml: -2.75, ...(hiddenSm ? {} : { mr: 3.5 }) }}
+          >
+            <Menu />
+          </IconButton>
+        ) : null}
+      </Box>
       <Box className='actions-right' sx={{ display: 'flex', alignItems: 'center' }}>
         <ModeToggler settings={settings} saveSettings={saveSettings} />
         <UserDropdown />
