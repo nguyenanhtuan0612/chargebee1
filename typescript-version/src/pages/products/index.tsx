08195fa53b2fc678b8916cc680b0e1b12fb9f25d
ha.le <baopt>
2023-01-15 16:47:51 +0700

    update product

--- typescript-version/src/pages/products/index.tsx
+++ typescript-version/src/pages/products/index.tsx
@@ -1,5 +1,15 @@
 // ** MUI Imports
-import { Backdrop, CircularProgress, Pagination } from '@mui/material';
+import {
+  Backdrop,
+  Box,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Pagination,
+  Select,
+  SelectChangeEvent
+} from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
@@ -16,7 +26,7 @@ const Product = () => {
 
   const [exchangeRate, setExchangRate] = useState(0);
   const [discountForColaborator, setDiscountForColaborator] = useState(0);
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<number | undefined>(undefined);
   const [page, setPage] = useState<number>(1);
   const [rowsPerPage] = useState(12);
   const [totalPage, setTotalPage] = useState<number>(0);
@@ -34,11 +44,33 @@ const Product = () => {
 
     const token = localStorage.getItem('token') || '';
     const url = `${process.env.apiUrl}/api/tiktokAccount/listTiktokAccountCoin`;
+    let params: { limit: number; offset: number; filter?: string } = {
+      limit: rowsPerPage,
+      offset: (page - 1) * rowsPerPage
+    };
+    if (valueSortCoin) {
+      let filter;
+      switch (valueSortCoin) {
+        case '1':
+          filter = JSON.stringify([{ operator: 'between', value: '[0, 1000]', prop: 'tiktokCoin' }]);
+          params = { ...params, filter };
+          break;
+        case '2':
+          filter = JSON.stringify([{ operator: 'between', value: '[1000, 2000]', prop: 'tiktokCoin' }]);
+          params = { ...params, filter };
+          break;
+        case '3':
+          filter = JSON.stringify([{ operator: 'between', value: '[1000, 2000]', prop: 'tiktokCoin' }]);
+          params = { ...params, filter };
+          break;
+        case '4':
+          filter = JSON.stringify([{ operator: 'between', value: '[5000]', prop: 'tiktokCoin' }]);
+          params = { ...params, filter };
+          break;
+      }
+    }
     const config = {
-      params: {
-        limit: rowsPerPage,
-        offset: (page - 1) * rowsPerPage
-      },
+      params,
       headers: {
         Authorization: `Bearer ${token}`
       }
@@ -66,12 +98,41 @@ const Product = () => {
     setChange(!change);
   };
 
-  return (
+  const [valueSortCoin, setValueSortCoin] = useState('');
+
+  const handleChangeSortCoin = (event: SelectChangeEvent) => {
+    setValueSortCoin(event.target.value as string);
+    setChange(!change);
+  };
+
+  return count === undefined ? (
+    <Grid>Hiện tại chưa có sản phẩm nào</Grid>
+  ) : (
     <Grid container spacing={6}>
       <Grid item xs={12} sx={{ paddingBottom: 4 }}>
         <Typography variant='h6'>{'Danh sách tài khoản Tiktok'}</Typography>
       </Grid>
-      <Grid item xs={12} sx={{ justifyContent: 'space-between' }}>
+      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        {/* <Box sx={{ minWidth: 120 }}>
+          <FormControl size='small'>
+            <InputLabel id='demo-simple-select-label'>Sắp xếp xu</InputLabel>
+            <Select
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              value={valueSortCoin}
+              label='Sắp xếp xu'
+              onChange={handleChangeSortCoin}
+            >
+              <MenuItem value=''>
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={'1'}>Từ 0 xu đến 1000 xu</MenuItem>
+              <MenuItem value={'2'}>Từ 1000 xu đến 2000 xu</MenuItem>
+              <MenuItem value={'3'}>Từ 2000 xu đến 5000 xu</MenuItem>
+              <MenuItem value={'4'}>Lớn hơn 5000 xu</MenuItem>
+            </Select>
+          </FormControl>
+        </Box> */}
         <Pagination count={totalPage} page={page} onChange={handleChangePage} />
       </Grid>
       {listAccounts.map((item, index) => (
